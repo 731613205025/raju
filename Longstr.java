94af73a4ea941e711fbc43db6d5047799d0a6971
731613205025 <maniharish95@gmail.com>
2016-07-18 11:24:43 +0530

    Add files via upload

---
+++ Longstr.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package longstr;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Student
+ */
+public class Longstr {
+
+    
+    
+        
+    public static void main(String args[]) {
+          Scanner sc = new Scanner(System.in);
+ 
+        System.out.print("Enter a string: ");
+        String st = sc.nextLine();
+ 
+        String word = "";
+        String largeWord = "";
+        int i, l, lw, max = 0, c = 0;
+        char ch;
+        st = st + " ";
+        l = st.length();
+        for (i = 0; i < l; i++) {
+            ch = st.charAt(i);
+            if (ch != ' ') {
+                word = word + ch;
+            } else {
+                lw = word.length();
+                if (lw > max) {
+                    max = lw;
+                    largeWord = word;
+                }
+                word = "";
+            }
+        }
+        System.out.println("Largest Word: " + largeWord);    
+}
+    }
+
